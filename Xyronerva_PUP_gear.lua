fa70e76423548cc90cd7d2337408a6bd3e978368
Aeromehl <Bosshog1984@gmail.com>
2016-12-18 11:53:20 -0600

    Add files via upload

--- Xyronerva_PUP_gear.lua
+++ Xyronerva_PUP_gear.lua
@@ -1,56 +1,65 @@
 function init_gear_sets()
+	
+	
+-- Cory ----------------------------------------------------------------------------	
+-- You need to make a macro in game For Gear Set Swaps.
+-- Hitting this macro will cycle between your standard/pdt/mdt sets for idle/engaged
+-- Line 1 /console gs c cycle idle
+-- Line 2 /console gs c cycle offense	
+-- ---------------------------------------------------------------------------------	
+	
 	--============================================================
 	--						Precast sets(Magic)
 	--============================================================
-	sets.precast.FC = {main="Oatixur",
-				head="Pitre Taj +1",
-				neck="Magoraga Beads",
-				ear1="Loquac. Earring",
-				ear2="Bladeborn Earring",
-				body="Thaumas Coat",
-				hands="Regimen Mittens",
-				ring1="Epona's Ring",
-				ring2="Prolix Ring",
-				back="Swith Cape",
-				waist="Hurch'lan Sash",
-				legs="Orvail Pants",
-				feet="Chelona Boots",
-				range="Em. Animator",
-				ammo="Automat. Oil +3"}
-	sets.precast.FC.ElementalMagic = {}
-	sets.precast.FC.DarkMagic = {}
-	sets.precast.FC.EnfeeblingMagic = {}
-	sets.precast.FC.DivineMagic = {}
-	sets.precast.FC.BlueMagic = {}
-	sets.precast.FC.BarElement = {}
-	sets.precast.FC.StatusRemoval = {}
-	sets.precast.FC.SummoingMagic = {}
+	sets.precast.FC = {head="Pitre Taj +1",
+    body="Qaaxo Harness",
+    hands="Foire Dastanas +1",
+    legs="Qaaxo Tights",
+    feet="Foire Bab. +1",
+    waist="Hurch'lan Sash",
+    right_ear="Loquac. Earring",
+    back="Contriver's Cape",}
+	
 	sets.precast.FC.NinjutsuMagic = {}
-	sets.precast.FC.Utsusemi = set_combine(sets.precast.FC, {})
-	sets.precast.FC.SingingMagic = {}
-	sets.precast.FC.WindMagic = {}
-	sets.precast.FC.StringMagic = {}	
+	sets.precast.FC.Utsusemi = {head="Pitre Taj +1",
+    body="Qaaxo Harness",
+    hands="Foire Dastanas +1",
+    legs="Qaaxo Tights",
+    feet="Foire Bab. +1",
+    neck="Magoraga Beads",
+    waist="Hurch'lan Sash",
+    right_ear="Loquac. Earring",
+    back="Contriver's Cape"}
+	
+		
 	sets.precast.FC.EnhancingMagic = {}
 	sets.precast.FC.EnhancingMagic.Stoneskin = {}
+	
 	sets.precast.FC.HealingMagic = {}
+	
 	sets.precast.FC.Cure = {}
-	sets.precast.FC.Curaga = sets_combine(sets.precast.FC.Cure, {})
+	sets.precast.FC.Curaga = sets.precast.FC.Cure
 	
 	
 	--============================================================
 	--						Precast sets(Abilities)
 	--============================================================
 	
-	sets.precast.JA.Maneuver = {neck="Bfn. Collar +1",body="Cirque Farsetto +1",
-								hands={'Foire Dastanas +1', 'Foire Dastanas'},range="Eminent Animator"}
-	sets.precast.JA['Repair'] = {ear1="Ghignol Earring",feet={'Foire Bab. +1', 'Foire Babouches'}}
-	sets.precast.JA['Overdrive'] = {body={'Pitre Tobe+1', 'Pitre Tobe'}}
-	sets.precast.JA['Activate'] = {}
+	sets.precast.JA.Maneuver = {head="Murzim Zucchetto",
+								neck={"Buffoon's Collar +1","Buffoon's Collar"},
+								body={"Karagoz Farsetto +1", "Karagoz Farsetto", "Cirque Farsetto +1", "Cirque Farsetto"},
+								hands={'Foire Dastanas +1', 'Foire Dastanas', "Puppetry Dastanas +1", "Puppetry Dastanas"},
+								range="Divinator",
+								waist="Hurch'lan Sash",
+								back={"Visucius's Mantle", 'Dispersal Mantle'}}
+	sets.precast.JA['Repair'] = {ear1="Ghignol Earring",ear2="Pratik Earring",feet={'Foire Babouches +1', 'Puppetry Babouches +1', 'Foire Babouches',  'Puppetry Babouches'}}
+	sets.precast.JA['Overdrive'] = {body={'Pitre Tobe +2', 'Pitre Tobe +1', 'Pitre Tobe'}}
+	sets.precast.JA['Activate'] = {back="Visucius's Mantle"}
 	sets.precast.JA['Deus Ex Automata'] = {}
 	sets.precast.JA['Maintenance'] = {}
-	sets.precast.JA['Role Reversal'] = {feet={'Pitre Babouches +1', 'Pitre Babouches'}}
-	sets.precast.JA['Ventriloquy'] = {legs={'Pitre Curidars+1', 'Pitre Churidars'}}
-	sets.precast.JA['Tactical Switch'] = {feet='Cirque Scarpe +2'}
+	sets.precast.JA['Role Reversal'] = {feet={'Pitre Babouches +2', 'Pitre Babouches +1', 'Pitre Babouches'}}
+	sets.precast.JA['Ventriloquy'] = {legs={'Pitre Churidars +2', 'Pitre Churidars +1', 'Pitre Churidars'}}
+	sets.precast.JA['Tactical Switch'] = {feet={'Karagoz Scarpe +1','Karagoz Scarpe', 'Cirque Scarpe +2', 'Cirque Scarpe +1'}}
 	sets.precast.JA['Cooldown'] = {}
 	sets.precast.JA['Heady Artifice'] = {}
 		
@@ -64,164 +73,111 @@ function init_gear_sets()
 	--						Precast sets(Weapon Skills)
 	--============================================================
 	
-	sets.wsBase = {head="Uk'uxkaj Cap",
-				neck="Love torque",
-				ear1="Kemas Earring",
-				ear2="Thunder Pearl",
-				body="Otronif Harness",
-				hands="Regimen Mittens",
-				ring1="Pyrosoul Ring",
-				ring2="Pyrosoul Ring",
-				back="Buquwik Cape",
-				waist="Pipilaka Belt",
-				legs="Quiahuiz Trousers",
-				feet="Otronif Boots",
-				range="Em. Animator"}
-	sets.wsBase.STRVIT = {head="Uk'uxkaj Cap",
-				neck="Love torque",
-				ear1="Kemas Earring",
-				ear2="Thunder Pearl",
-				body="Otronif Harness",
-				hands="Regimen Mittens",
-				ring1="Pyrosoul Ring",
-				ring2="Pyrosoul Ring",
-				back="Buquwik Cape",
-				waist="Pipilaka Belt",
-				legs="Quiahuiz Trousers",
-				feet="Otronif Boots",
-				range="Em. Animator"}
-	sets.wsBase.STR = {head="Uk'uxkaj Cap",
-				neck="Love torque",
-				ear1="Kemas Earring",
-				ear2="Thunder Pearl",
-				body="Otronif Harness",
-				hands="Regimen Mittens",
-				ring1="Pyrosoul Ring",
-				ring2="Pyrosoul Ring",
-				back="Buquwik Cape",
-				waist="Pipilaka Belt",
-				legs="Quiahuiz Trousers",
-				feet="Otronif Boots",
-				range="Em. Animator"}
-	sets.wsBase.DEX = {}
-	sets.wsBase.STRDEX = {}
-	sets.wsBase.Magic = {}
+	sets.wsBase = {head={ name="Pitre Taj +1", augments={'Enhances "Optimization" effect',}},
+    body="Otro. Harness +1",
+    hands="Regimen Mittens",
+    legs={ name="Manibozho Brais", augments={'Attack+11','Accuracy+7','STR+3',}},
+    feet="Foire Bab. +1",
+    neck="Love Torque",
+    waist="Pipilaka Belt",
+    left_ear="Steelflash Earring",
+    right_ear="Bladeborn Earring",
+    left_ring="Epona's Ring",
+    right_ring="Rajas Ring",
+    back="Kayapa Cape",}
+	sets.wsBase.STRVIT = {head="Pitre Taj +1",
+    body="Otro. Harness +1",
+    hands="Regimen Mittens",
+    legs="Foire Churidars +1",
+    feet="Foire Bab. +1",
+    neck="Love Torque",
+    waist="Pipilaka Belt",
+    left_ear="Steelflash Earring",
+    right_ear="Bladeborn Earring",
+    left_ring="Epona's Ring",
+    right_ring="Rajas Ring",
+    back="Kayapa Cape",}
+	sets.wsBase.STR = {head="Pitre Taj +1", 
+    body="Otro. Harness +1",
+    hands="Regimen Mittens",
+    legs="Foire Churidars +1",
+    feet="Foire Bab. +1",
+    neck="Love Torque",
+    waist="Pipilaka Belt",
+    left_ear="Steelflash Earring",
+    right_ear="Bladeborn Earring",
+    left_ring="Epona's Ring",
+    right_ring="Rajas Ring",
+    back="Kayapa Cape",}
 	
 	--============================================================
 	--						Midcast sets
 	--============================================================
 	
-	sets.midcast.FastRecast = {main="Oatixur",
-				head="Pitre Taj +1",
-				neck="Asperity Necklace",
-				ear1="Steelflash Earring",
-				ear2="Bladeborn Earring",
-				body="Thaumas Coat",
-				hands="Regimen Mittens",
-				ring1="Epona's Ring",
-				ring2="Rajas Ring",
-				back="Contriver's Cape",
-				waist="Hurch'lan Sash",
-				legs="Quiahuiz Trousers",
-				feet="Otronif Boots",
-				range="Em. Animator",
-				ammo="Automat. Oil +3"}
+	sets.midcast.FastRecast = {}
 	
 	sets.midcast.MagicAccuracy = {}
 
-	--====================== Healing =============================
+	--============================================================
+	--	May not work		VVVVVVVV
+	--============================================================
+	--============================================================
+	sets.midcast.Pet.weaponskill = {body="Thaumas Coat"}
+	sets.midcast.Pet.ElementalMagic = {body="Thaumas Coat"}
+	sets.midcast.Pet.HealingMagic = {body="Thaumas Coat"}
+	sets.midcast.Pet.RA = {body="Thaumas Coat"}
+	--============================================================
+	--============================================================
+	--	May not work		^^^^^^^^
+	--============================================================
 	
-	sets.midcast.HealingMagic = {}
-	
-	sets.midcast.Cure = {}
-	sets.midcast.Cure.with_buff = {}
-
-	sets.midcast.Curaga = sets.midcast.Cure
-	sets.midcast.Cure.Engaged = sets.midcast.Cure
-
-	sets.midcast.Cursna = {}
-	sets.midcast.Cursna.with_buff['doom'] = {}
-
-	sets.midcast.StatusRemoval = {}
-	sets.midcast.StatusRemoval['Divine Caress'] = {}
-
-	--====================== Enhancing ===========================
-	
-	sets.midcast.EnhancingMagic = {}
-	sets.midcast.Stoneskin = {}
-	sets.midcast.Auspice = {}
-	sets.midcast.Aquaveil = {}
-	sets.midcast.BarElement = {}
-	sets.midcast.Regen = {}
-	sets.midcast.Protectra = {}
-	sets.midcast.Shellra = {}
-	
-	--====================== Offensive ===========================	
-	
-	sets.midcast.DivineMagic = {}
-	sets.midcast.DivineMagic.Nuke = {}
-	sets.midcast.DivineMagic.Nuke.with_buff = {}
-	
-	sets.midcast.DarkMagic = {}
-
-	sets.midcast.EnfeeblingMagic = {}
-	sets.midcast.EnfeeblingMagic.Potency = {}
-	sets.midcast.EnfeeblingMagic.Potency.Resistant = {}
-	sets.midcast.EnfeeblingMagic.Potency.Normal = {}
-	
-	sets.midcast.ElementalMagic = {}
-	sets.midcast.ElementalMagic.with_buff['elemental seal'] = {}
-	sets.midcast.ElementalMagic.Earth = {}
-	
-	sets.midcast.ElementalEnfeeble = {}
-	
-	sets.midcast.BlueMagic.Physical = {}
-	sets.midcast.BlueMagic.Physical.DEX = {}
-	sets.midcast.BlueMagic.Physical.STR = {}
-	
-	sets.midcast.BlueMagic.Magic = {}
-	sets.midcast.BlueMagic.Magic.DEX = {}
-	sets.midcast.BlueMagic.Magic.STR = {}
 	
 	--============================================================
 	--						Other sets
 	--============================================================
 	
-	sets.resting = set_combine(sets.idle, {Foire Taj})
+	sets.resting = {head="Foire Taj",
+    body="Foire Tobe",
+    legs="Foire Churidars +1",
+    back="Contriver's Cape",}
 	sets.resting.with_buff = {}
 
-	sets.idle = {main="Oatixur",				head="Pitre Taj +1",		neck="Asperity Necklace",
-			ear1="Steelflash Earring",	ear2="Bladeborn Earring",	body="Thaumas Coat",
-			hands="Regimen Mittens",	ring1="Epona's Ring",		ring2="Rajas Ring",
-			back="Contriver's Cape",	waist="Cetl Belt",			legs="Manibozho Brais",
-			feet="Foire Bab. +1",		range="Em. Animator",		ammo="Automat. Oil +3"}
+	sets.idle = {}
+	sets.idle.Standard = {head="Pitre Taj +1",
+    body="Foire Tobe",
+    hands="Regimen Mittens",
+    legs="Foire Churidars +1",
+    back="Contriver's Cape",}
+	sets.idle.PDT = {}
+	sets.idle.MDT = {}
+	
 	sets.idle.lowMP = {}
 	sets.idle.lowMP_night =	{}
 	sets.idle.lowMP_day = {}
 	
 	sets.idle.with_buff = {}
-	sets.idle.with_buff['doom'] = {}
+	sets.idle.with_buff['doom'] = {ring1={"Eshmun's Ring", "Saida Ring"}, ring2={"Eshmun's Ring", "Saida Ring"}, waist="Gishdubar Sash"}
+	sets.idle.with_buff['reive mark'] = {neck={"Adoulin's Refuge +1", "Ygnas's Resolve +1", "Arciela's Grace +1", "Adoulin's Refuge", "Ygnas's Resolve", "Arciela's Grace"}}
 	
-	sets.defense.DT = {}
-	sets.defense.PDT = combineSets(sets.defense.DT, {})
-	sets.defense.MDT = combineSets(sets.defense.DT, {})
+	sets.weapons = {main="Oatixur", Range="Eminent Animator II", Ammo={"Automaton Oil +3", "Automaton Oil +2", "Automaton Oil +1", "Automaton Oil"}}
 	
-	sets.engaged = {main="Oatixur",
-				head="Pitre Taj +1",
-				neck="Cirque Necklace",
-				ear1="Steelflash Earring",
-				ear2="Bladeborn Earring",
-				body="Otronif Harness",
-				hands="Regimen Mittens",
-				ring1="Epona's Ring",
-				ring2="Rajas Ring",
-				back="Contriver's Cape",
-				waist="Hurch'lan Sash",
-				legs="Quiahuiz Trousers",
-				feet="Otronif Boots",
-				range="Em. Animator",
-				ammo="Automat. Oil +3"}
-	sets.engaged.with_buff['doom'] = {}
+	sets.engaged = {}
+	sets.engaged.Standard = {head="Pitre Taj +1",
+    body="Qaaxo Harness",
+    hands="Regimen Mittens",
+    legs="Foire Churidars +1",
+    feet="Foire Bab. +1",
+    neck="Cirque Necklace",
+    waist="Hurch'lan Sash",
+    left_ear="Steelflash Earring",
+    right_ear="Bladeborn Earring",
+    left_ring="Epona's Ring",
+    right_ring="Rajas Ring",
+    back="Contriver's Cape",}
+	sets.engaged.PDT = {}
+	sets.engaged.MDT = {}
+	
 	sets.engaged.with_buff = {}
-	sets.engaged.with_buff['reive mark'] = {}
+	sets.engaged.with_buff['doom'] = {ring1={"Eshmun's Ring", "Saida Ring"}, ring2={"Eshmun's Ring", "Saida Ring"}, waist="Gishdubar Sash"}
+	sets.engaged.with_buff['reive mark'] = {neck={"Adoulin's Refuge +1", "Ygnas's Resolve +1", "Arciela's Grace +1", "Adoulin's Refuge", "Ygnas's Resolve", "Arciela's Grace"}}
 end
